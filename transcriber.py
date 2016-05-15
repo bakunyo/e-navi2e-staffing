a9afcecb603cf831dba85b4522587864204e9ca3
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-05-15 19:36:20 +0900

    Add transcriber, and login e-navi

---
+++ transcriber.py
@@ -0,0 +1,19 @@
+import json
+from selenium import webdriver
+
+config = json.load(open('config.json', 'r'))
+
+driver = webdriver.Firefox()
+driver.maximize_window()
+
+# e-navi login
+login_url = 'https://www.enavi-ts.net/ts-h-staff/Staff/login.aspx'
+company_id = config['e-navi']['companyId']
+staff_no = config['e-navi']['staffNo']
+password = config['e-navi']['password']
+
+driver.get(login_url + '?ID=' + company_id)
+login_form = driver.find_element_by_id('inputid')
+login_form.find_element_by_name('TextStaffNo').send_keys(staff_no)
+login_form.find_element_by_name('TextPassword').send_keys(password)
+login_form.find_element_by_name('BtnOk').click()
