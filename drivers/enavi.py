892b7a656d8c1181a8870925d9558a3bd7154e55
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-05-20 02:32:02 +0900

    Move login to ENavi class

---
+++ drivers/enavi.py
@@ -0,0 +1,50 @@
+class ENavi:
+  def __init__(self, driver, config):
+    self.driver = driver
+    self.company_id = config['companyId']
+    self.url_dir = config['urlDir']
+    self.staff_no = config['staffNo']
+    self.password = config['password']
+
+  def login_url(self):
+    url = 'https://www.enavi-ts.net/ts-h-staff/Staff/login.aspx'
+    return url + '?ID=' + self.company_id
+
+  def login(self):
+    self.driver.get(self.login_url())
+
+    login_form = self.driver.find_element_by_id('inputid')
+    login_form.find_element_by_name('TextStaffNo').send_keys(self.staff_no)
+    login_form.find_element_by_name('TextPassword').send_keys(self.password)
+    login_form.find_element_by_name('BtnOk').click()
+
+  def move_monthly_page(self):
+    monthly_url = 'https://web.enavi-ts.net/' + self.url_dir + '/Staff/month/monthwork.aspx'
+    self.driver.get(monthly_url)
+
+  def days(self):
+    timesheet = self.driver.find_element_by_id('TableTimesheet')
+    return timesheet.find_elements_by_tag_name('tr')
+
+  def cells_by_day(self):
+    return [day.find_elements_by_tag_name('td') for day in self.days()]
+
+  def get_timesheet(self):
+    self.login()
+    self.move_monthly_page()
+
+    columns = { 'date': 1, 'status': 4, 'attend': 6, 'begin': 7, 'end': 8, 'break': 9, 'basic': 10, 'extra': 11 }
+    enavi_list = []
+
+    for day_cells in self.cells_by_day():
+      date = day_cells[columns['date']].text
+      if date == '日付': continue
+
+      key = int(date.split('/')[1])
+      day = {}
+      [day.update({ k: day_cells[v].text}) for k, v in columns.items()]
+
+      if any([day['status'] == s for s in ['承認済', '依頼中']]):
+        enavi_list.append({ 'date': key, 'time': day })
+
+    return enavi_list
--- transcriber.py
+++ transcriber.py
@@ -1,44 +1,15 @@
 import json
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from drivers.enavi import *
 
 config = json.load(open('config.json', 'r'))
 
 driver = webdriver.Firefox()
 driver.maximize_window()
 
-# e-navi login
-login_url = 'https://www.enavi-ts.net/ts-h-staff/Staff/login.aspx'
-company_id = config['e-navi']['companyId']
-staff_no = config['e-navi']['staffNo']
-password = config['e-navi']['password']
-urlDir = config['e-navi']['urlDir']
+enavi_timesheet = ENavi(driver, config['e-navi']).get_timesheet()
 
-driver.get(login_url + '?ID=' + company_id)
-login_form = driver.find_element_by_id('inputid')
-login_form.find_element_by_name('TextStaffNo').send_keys(staff_no)
-login_form.find_element_by_name('TextPassword').send_keys(password)
-login_form.find_element_by_name('BtnOk').click()
-
-# open monthly page
-driver.get('https://web.enavi-ts.net/' + urlDir + '/Staff/month/monthwork.aspx');
-
-timesheet = driver.find_element_by_id('TableTimesheet')
-days = timesheet.find_elements_by_tag_name('tr')
-
-columns = { 'date': 1, 'status': 4, 'attend': 6, 'begin': 7, 'end': 8, 'break': 9, 'basic': 10, 'extra': 11 }
-enavi_list = []
-cells_by_day = [day.find_elements_by_tag_name('td') for day in days]
-for day_cells in cells_by_day:
-  date = day_cells[columns['date']].text
-  if date == '日付': continue
-
-  key = int(date.split('/')[1])
-  day = {}
-  [day.update({ k: day_cells[v].text}) for k, v in columns.items()]
-
-  if any([day['status'] == s for s in ['承認済', '依頼中']]):
-    enavi_list.append({ 'date': key, 'time': day })
 
 # e-staffing
 login_url = config['e-staffing']['loginUrl']
