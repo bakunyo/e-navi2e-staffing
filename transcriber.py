e2e73f21419fa0b9bd5ca27f5cd945bf36c474d7
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-05-19 15:24:01 +0900

    Use config urlDir

--- transcriber.py
+++ transcriber.py
@@ -12,6 +12,7 @@ login_url = 'https://www.enavi-ts.net/ts-h-staff/Staff/login.aspx'
 company_id = config['e-navi']['companyId']
 staff_no = config['e-navi']['staffNo']
 password = config['e-navi']['password']
+urlDir = config['e-navi']['urlDir']
 
 driver.get(login_url + '?ID=' + company_id)
 login_form = driver.find_element_by_id('inputid')
@@ -20,7 +21,7 @@ login_form.find_element_by_name('TextPassword').send_keys(password)
 login_form.find_element_by_name('BtnOk').click()
 
 # open monthly page
-driver.get('https://web.enavi-ts.net/cs-staff-rks/Staff/month/monthwork.aspx');
+driver.get('https://web.enavi-ts.net/' + urlDir + '/Staff/month/monthwork.aspx');
 
 timesheet = driver.find_element_by_id('TableTimesheet')
 days = timesheet.find_elements_by_tag_name('tr')
